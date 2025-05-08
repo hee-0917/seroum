ca3d4365b4727df33ad2685843604a29c74ecb8c
hee-0917 <nymph4276@nate.com>
2025-05-08 16:16:23 +0900

    Delete app directory

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'SEROUM',
-  description: 'SEROUM 공식 웹사이트',
-  generator: 'Next.js',
-} 
